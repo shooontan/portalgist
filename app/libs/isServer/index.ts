eaf1e82541e97f3d33b2f4afdce6a304d41757b0
shooontan <logo.bee.doo19@gmail.com>
2018-10-13 20:05:20 +0900

    add isServer test

---
+++ app/libs/isServer/index.ts
@@ -0,0 +1,2 @@
+import isServer from './isServer';
+export default isServer;
---
+++ app/libs/isServer/isServer.test.ts
@@ -0,0 +1,5 @@
+import isServer from './isServer';
+
+test('isServer should be boolean', () => {
+  expect(typeof isServer).toBe('boolean');
+});
