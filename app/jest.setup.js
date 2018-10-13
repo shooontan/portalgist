1b8d83e6583c52ad94a83d19cdc5618e9087339a
shooontan <logo.bee.doo19@gmail.com>
2018-10-14 04:03:24 +0900

    fix jest setup for next/config

--- app/jest.setup.js
+++ app/jest.setup.js
@@ -1,2 +1,13 @@
 require('jest-dom/extend-expect');
 require('react-testing-library/cleanup-after-each');
+
+/**
+ * if no this config, warn error following.
+ *
+ * TypeError: Cannot read property 'publicRuntimeConfig' of undefined
+ *
+ * https://github.com/zeit/next.js/issues/4024
+ */
+const { setConfig } = require('next/config');
+const { publicRuntimeConfig } = require('./next.config');
+setConfig({ publicRuntimeConfig });
