8dbb39a6a189f6acb08266576d5274c661322721
shooontan <logo.bee.doo19@gmail.com>
2018-10-23 08:17:05 +0900

    tweak

--- app/libs/firebase/Firebase.tsx
+++ app/libs/firebase/Firebase.tsx
@@ -7,13 +7,6 @@ const { publicRuntimeConfig: prc } = getConfig();
 const config = {
   apiKey: process.env.FIREBASE_API_KEY || prc.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN || prc.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL || prc.FIREBASE_DATABASE_URL,
-  messagingSenderId:
-    process.env.FIREBASE_MESSAGING_SENDER_ID ||
-    prc.FIREBASE_MESSAGING_SENDER_ID,
-  projectId: process.env.FIREBASE_PROJECT_ID || prc.FIREBASE_PROJECT_ID,
-  storageBucket:
-    process.env.FIREBASE_STORAGE_BUCKET || prc.FIREBASE_STORAGE_BUCKET,
 };
 
 if (!firebase.apps.length) {
